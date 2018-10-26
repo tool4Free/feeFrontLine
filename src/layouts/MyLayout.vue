f745831668a5043ab5cda5e22a08533daecf14c0
baram204 <baram204@gmail.com>
2018-10-27 01:00:45 +0900

    feeFrontLine-23 입력한 결과를 원하는 대로 볼 수 있는 화면을 제공해
    사용자가 원청이름으로 검색한 후 테이블의 열 정렬 기능을 사용해 자신과
    같거나 유사한 조건의 계약이 얼마를 받았는지 볼 수있게 하려고 테이블과
    검색 컴포넌트를 적용했고 테이블 정보 중 원치 않는 정보는 빼기 위해서
    테이블 열 컴포넌트를 적용.

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -17,8 +17,8 @@
         </q-btn>
 
         <q-toolbar-title>
-          Quasar App
-          <div slot="subtitle">Running on Quasar v{{ $q.version }}</div>
+          아단전
+          <div slot="subtitle">아이티 프리랜서 단가 전선</div>
         </q-toolbar-title>
       </q-toolbar>
     </q-layout-header>
@@ -32,27 +32,31 @@
         link
         inset-delimiter
       >
-        <q-list-header>Essential Links</q-list-header>
-        <q-item @click.native="openURL('http://quasar-framework.org')">
-          <q-item-side icon="school" />
-          <q-item-main label="Docs" sublabel="quasar-framework.org" />
-        </q-item>
+        <q-list-header>아단전 링크</q-list-header>
+        <!--<q-item @click.native="openURL('http://quasar-framework.org')">-->
+          <!--<q-item-side icon="school" />-->
+          <!--<q-item-main label="Docs" sublabel="quasar-framework.org" />-->
+        <!--</q-item>-->
         <q-item @click.native="openURL('https://github.com/quasarframework/')">
           <q-item-side icon="code" />
-          <q-item-main label="GitHub" sublabel="github.com/quasarframework" />
+          <q-item-main label="툴포프리" sublabel="github.com/tool4free" />
         </q-item>
-        <q-item @click.native="openURL('https://discord.gg/5TDhbDg')">
-          <q-item-side icon="chat" />
-          <q-item-main label="Discord Chat Channel" sublabel="https://discord.gg/5TDhbDg" />
-        </q-item>
-        <q-item @click.native="openURL('http://forum.quasar-framework.org')">
+        <q-item @click.native="openURL('https://github.com/tool4Free/support')">
           <q-item-side icon="record_voice_over" />
-          <q-item-main label="Forum" sublabel="forum.quasar-framework.org" />
+          <q-item-main label="문의 및 지원" sublabel="github.com/tool4Free/support" />
         </q-item>
-        <q-item @click.native="openURL('https://twitter.com/quasarframework')">
-          <q-item-side icon="rss feed" />
-          <q-item-main label="Twitter" sublabel="@quasarframework" />
+        <q-item @click.native="openURL('https://open.kakao.com/o/geHO1CL')">
+        <q-item-side icon="chat" />
+        <q-item-main label="카카오톡 오픈채팅방" sublabel="https://discord.gg/x5GUZhA" />
         </q-item>
+        <!--<q-item @click.native="openURL('https://discord.gg/x5GUZhA')">-->
+          <!--<q-item-side icon="chat" />-->
+          <!--<q-item-main label="디스코드 채팅 채널" sublabel="https://discord.gg/x5GUZhA" />-->
+        <!--</q-item>-->
+        <!--<q-item @click.native="openURL('https://twitter.com/quasarframework')">-->
+          <!--<q-item-side icon="rss feed" />-->
+          <!--<q-item-main label="Twitter" sublabel="@quasarframework" />-->
+        <!--</q-item>-->
       </q-list>
     </q-layout-drawer>
 
